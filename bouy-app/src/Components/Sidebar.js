646798b6e89215edc60024f0ce1e440981c715bd
Aj10centz <adamjsteinbacher@gmail.com>
2020-12-08 01:05:00 -0600

    basic css created for sidbar component, so that functionality can be enabled

--- bouy-app/src/Components/Sidebar.js
+++ bouy-app/src/Components/Sidebar.js
@@ -1,15 +1,37 @@
 import React from "react";
 import "../Components/Sidebar.css";
+import ForumIcon from "@material-ui/icons/Forum";
+import { IconButton } from "@material-ui/core";
+import PersonIcon from "@material-ui/icons/Person";
+import DragIndicatorIcon from "@material-ui/icons/DragIndicator";
+import DonutSmallIcon from "@material-ui/icons/DonutSmall";
+import FindReplaceIcon from "@material-ui/icons/FindReplace";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <div className="sidebar__header">
-        
-        <div className="sidebar__headerRight">
-            
-
+      <div className="sidebarHeader">
+        <PersonIcon />
+        <div className="sidebarHeaderRight">
+          <IconButton>
+            <DonutSmallIcon />
+          </IconButton>
+          <IconButton>
+            <ForumIcon />
+          </IconButton>
+          <IconButton>
+            <DragIndicatorIcon />
+          </IconButton>
         </div>
+      </div>
+      <div className="sidebarSearch">
+        <div className="searchContainer">
+          <FindReplaceIcon />
+          <input placeholder="Search for a room, or start a new chat" />
+        </div>
+      </div>
+      <div className="sidebarChats">
+          
       </div>
     </div>
   );
