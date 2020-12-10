79537c0ef123cc750cd3106d7c5a6a47e540e95f
Aj10centz <adamjsteinbacher@gmail.com>
2020-12-09 18:33:25 -0600

    index.js set up with health check api that works in postman

--- Server/index.js
+++ Server/index.js
@@ -1,12 +1,32 @@
 import express from 'express';
+import mongoose from "mongoose";
+import { start } from 'repl';
 
 
 
 
+const config = ("config");
+const app = express();
+const port = process.env.PORT || 5000;
 
-const app = express()
-const port = process.env.PORT || 5000
+
+function connectDB() {
+  mongoose
+    .connect(
+        config.get("mongoURI"), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("Connected to MongoDB..."))
+    .catch((err) => {
+      console.log(`Could not connect to MongoDB. ERROR: ${err}`);
+      process.exit(1);
+    });
+}
+app.get("/", (req,res) => res.status(200).send("hello world"));
+app.listen(port, () => console.log(`listening on localhost:${port}`));
 
 
+connectDB;
 
-app.listen(port,()=>console.log(`listening on localhost:${port}`))
+
