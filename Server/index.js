adc5afcde3062a1d334a68764e8f4bbe55ba8b7b
Aj10centz <adamjsteinbacher@gmail.com>
2020-12-09 19:23:17 -0600

    test

--- Server/index.js
+++ Server/index.js
@@ -1,32 +1,17 @@
-import express from 'express';
+import express from "express";
 import mongoose from "mongoose";
-import { start } from 'repl';
 
 
-
-
-const config = ("config");
 const app = express();
 const port = process.env.PORT || 5000;
 
 
-function connectDB() {
-  mongoose
-    .connect(
-        config.get("mongoURI"), {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then(() => console.log("Connected to MongoDB..."))
-    .catch((err) => {
-      console.log(`Could not connect to MongoDB. ERROR: ${err}`);
-      process.exit(1);
-    });
-}
-app.get("/", (req,res) => res.status(200).send("hello world"));
+
+mongoose.connect(MongoURI, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.get("/", (req, res) => res.status(200).send("hello world"));
 app.listen(port, () => console.log(`listening on localhost:${port}`));
-
-
-connectDB;
-
-
