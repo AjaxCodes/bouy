7f4f1db20119fd63298e651e604738ab63364f4c
Aj10centz <adamjsteinbacher@gmail.com>
2020-12-09 22:28:22 -0600

    pusher installed, messages post and get created and iplemented

--- Server/index.js
+++ Server/index.js
@@ -1,17 +1,46 @@
 import express from "express";
 import mongoose from "mongoose";
+import Messages from "./DbMessages.js";
 
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.json)
 
+const key = 
+"mongodb+srv://ajaxCodes:GhostRecon1121@cluster0.pywev.mongodb.net/BoutChatApp?retryWrites=true&w=majority";
 
-mongoose.connect(MongoURI, {
+mongoose.connect(key, {
   useCreateIndex: true,
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 app.get("/", (req, res) => res.status(200).send("hello world"));
-app.listen(port, () => console.log(`listening on localhost:${port}`));
+
+
+app.get('/messages/sync', (req, res) => {
+    Messages.find((err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(200).send(data)
+        }
+    })
+})
+
+app.post('/messages/new', (req,res) => {
+    const dbMessage = req.body
+
+    Messages.create(dbMessage, (err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(201).send(`new message created: \n ${data}`)
+        }
+    })
+})
+
+
+app.listen(port, () => console.log(`listening on port 5000:${port}`));
