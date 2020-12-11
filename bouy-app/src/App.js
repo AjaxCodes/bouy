76307988be2eef48a1231f3b5f58bfbd8941d9be
Aj10centz <adamjsteinbacher@gmail.com>
2020-12-11 02:52:02 -0600

    fixed back end

--- bouy-app/src/App.js
+++ bouy-app/src/App.js
@@ -46,14 +46,5 @@ export default App;
 
 
 
-import mongoose from 'mongoose';
 
-const messagesSchema = mongoose.Schema({
-    message: String,
-    name: String,
-    timestamp: String,
-    received: Boolean
-    
-});
 
-export default mongoose.model('messagecontents',messagesSchema);
