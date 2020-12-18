ccacfca74d3e1e30d9e5efc7038b717501d05a49
Aj10centz <adamjsteinbacher@gmail.com>
2020-12-18 16:19:24 -0600

    tshirts added to images folder for storefront, all un needed files deleted

--- bouy-app/src/Components/Message.js
+++ bouy-app/src/Components/Message.js
@@ -4,9 +4,11 @@ import "./Message.css";
 function Message({ message, timestamp, user, userImage }) {
   return (
     <div className="message">
-      <img src={userImage} alt="" />
+      <img src={userImage} alt=""/>
       <div className="messageInfo">
-        <h4>{user}</h4>
+        <h4>{user}{" "}
+         <span className="messageTimestamp"> {new Date(timestamp?.toDate()).toUTCString()} </span>
+        </h4>
         <p>{message}</p>
       </div>
     </div>
--- bouy-app/src/Pages/LandingPage.js
+++ bouy-app/src/Pages/LandingPage.js
@@ -6,8 +6,45 @@ function LandingPage() {
     <div>
 
 <Header/>
-        <h1> landing page </h1>
+
+<div classname="resources">
+  <h1>
+  The Suicide Prevention Resource Center (SPRC), the only federally supported resource center devoted to advancing the implementation of the National Strategy for Suicide Prevention. SPRC is funded by the U.S. Department of Health and Human Services’ Substance Abuse and Mental Health Services Administration (SAMHSA).
+  </h1>
+</div>
+<hr/>
+<div>
+  <h1>Preventing Suicide: A Technical Package of Policy, Programs, and Practices, a CDC resource for communities and states.</h1>
+
+
+
+</div>
+<hr/>
+<div classname="secondContainer">
+  <h1>How to Help Someone Thinking of Suicide, resources from the NLM available in English, Arabic, Burmese, French, Hakha Chin, Karen, Nepali, and Swahili</h1>
+
+
+
+</div>
+<hr/>
+<div className="thirdContainer">
+  <h1> National Suicide Prevention Lifeline: The Lifeline provides 24-hour, toll-free, and confidential support to anyone in suicidal crisis or emotional distress. Call 1-800-273-TALK (8255) to connect with a skilled, trained counselor at a crisis center in your area. Support is available in English and Spanish and via live chat.
+Disaster Distress Hotline: People affected by any disaster or tragedy can call this helpline, sponsored by SAMHSA, to receive immediate counseling. Call 1-800-985-5990 to connect with a trained professional from the closest crisis counseling center within the network.
+Veterans Crisis Line: This helpline is a free, confidential resource for Veterans of all ages and circumstances. Call 1-800-273-8255, press "1"; text 838255; or chat online to connect with 24/7 support.
+Crisis Text Line: Text HELLO to 741741 for free and confidential support 24 hours a day throughout the U.S</h1>
+
+</div>
+<hr/>
+<div classname="fourthContainer">
+        <h1> ASK: “Are you thinking about killing yourself?” It’s not an easy question, but studies show that asking at-risk individuals if they are suicidal does not increase suicides or suicidal thoughts.
+KEEP THEM SAFE: Reducing a suicidal person’s access to highly lethal items or places is an important part of suicide prevention. While this is not always easy, asking if the at-risk person has a plan and removing or disabling the lethal means can make a difference.
+BE THERE: Listen carefully and learn what the individual is thinking and feeling. Research suggests acknowledging and talking about suicide may reduce rather than increase suicidal thoughts.
+HELP THEM CONNECT: Save the National Suicide Prevention Lifeline’s (1-800-273-TALK (8255)) and the Crisis Text Line’s number (741741) in your phone, so it’s there when you need it. You can also help make a connection with a trusted individual like a family member, friend, spiritual advisor, or mental health professional.
+STAY CONNECTED: Staying in touch after a crisis or after being discharged from care can make a difference. Studies have shown the number of suicide deaths goes down when someone follows up with the at-risk person.</h1>
      </div>
+     </div>
+
+    
   );
 }
 
